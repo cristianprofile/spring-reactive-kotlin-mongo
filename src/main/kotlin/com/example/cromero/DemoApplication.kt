572627db118782ee597bc1239c87bc000781d47d
Cristian Romero Matesanz <cristianprofile@gmail.com>
2020-03-21 13:09:36 +0100

    Deleted unused import

--- src/main/kotlin/com/example/cromero/DemoApplication.kt
+++ src/main/kotlin/com/example/cromero/DemoApplication.kt
@@ -2,7 +2,6 @@ package com.example.cromero
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import reactor.blockhound.BlockHound
 
 @SpringBootApplication
 class DemoApplication
