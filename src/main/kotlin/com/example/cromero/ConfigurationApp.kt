07a83a3e8aab08a967920c753cc7c22b2b16c769
Cristian Romero Matesanz <cristianprofile@gmail.com>
2020-03-21 13:03:23 +0100

    Updated travis configuration

--- src/main/kotlin/com/example/cromero/ConfigurationApp.kt
+++ src/main/kotlin/com/example/cromero/ConfigurationApp.kt
@@ -4,7 +4,6 @@ package com.example.cromero
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
-import java.nio.charset.Charset
 
 
 @Configuration
